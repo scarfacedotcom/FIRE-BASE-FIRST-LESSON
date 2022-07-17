c8a39edf97d3e4c539d579120b08dba46558c16d
scarfacedotcom <peteradaaku@gmail.com>
2022-07-17 15:11:55 +0100

    few changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { initializeApp } from 'firebase/app'
 import {
   getFirestore, collection, onSnapshot,
   addDoc, deleteDoc, doc,
-  query, where
+  query, where,
+  orderBy, serverTimestamp
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -24,7 +25,7 @@ const db = getFirestore()
 const colRef = collection(db, 'books')
 
 //queries
-const q = query(colRef, where("author", "==", "scar face"))
+const q = query(colRef, where("author", "==", "scar face"), orderBy("createdAt"))
 
 // real time collection data
 
@@ -57,6 +58,7 @@ addBookForm.addEventListener('submit', (e) => {
   addDoc(colRef, {
     title: addBookForm.title.value,
     author: addBookForm.author.value,
+    createdAt: serverTimestamp()
   })
   .then(() => {
     addBookForm.reset()
